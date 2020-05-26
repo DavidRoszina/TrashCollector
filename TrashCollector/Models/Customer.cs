8f8e033b85c248ecc02b493b73712e30444d556a
DavidRoszina <david.a.roszina@gmail.com>
2020-05-26 09:17:27 -0500

    Added User Role logic for ASP.NET Users

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,5 +17,9 @@ namespace TrashCollector.Models
         public double Balance;
 
         public string PickupDay;
+
+        [ForeignKey("IdentityUser")]
+        public string IdentityUserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
     }
 }
