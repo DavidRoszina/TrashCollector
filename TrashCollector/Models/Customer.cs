751dab32ef79a223f097927af8128d19a812c023
DavidRoszina <david.a.roszina@gmail.com>
2020-05-27 23:47:44 -0500

    Added address values to Customer and Employee

--- TrashCollector/Models/Customer.cs
+++ TrashCollector/Models/Customer.cs
@@ -13,12 +13,10 @@ namespace TrashCollector.Models
         [Key]
 
         public int id { get; set; }
-
         public string Name { get; set; }
-
         public double Balance { get; set; }
-
         public string PickupDay { get; set; }
+        public string Address { get; set; }
 
         [ForeignKey("IdentityUser")]
         public string IdentityUserId { get; set; }
--- TrashCollector/Models/Employee.cs
+++ TrashCollector/Models/Employee.cs
@@ -14,6 +14,7 @@ namespace TrashCollector.Models
 
         public int id { get; set; }
         public string Name { get; set; }
+        public string Address { get; set; }
 
         [ForeignKey("IdentityUser")]
         public string IdentityUserId { get; set; }
